cdc0051667bfd54d8c9aef2162a0a86303b6cb4b
Cameron Reed <cameronreedadjusting@gmail.com>
2020-04-03 09:41:39 -0700

    Trying to get code to render in react app in chrome

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
-import Dashboard from './Components/Dashboard';
-import Form from './Components/Form';
-import Header from './Components/Header';
-import Product from './Components/Product';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Form from './Components/Form/Form';
+import Header from './Components/Header/Header';
+import Product from './Components/Product/Product';
 
-function App() {
-  return (
-    <div className="App">
-      <div className='Dashboard'></div>
-      <div className='Form'></div>
-      <div className='Header'></div>
-      <div className='Product'></div>
-    </div>
-  );
+class App extends React.Component {
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className='Dashboard'>
+          <div className='Product'></div>
+        </div>
+        <div className='Form'></div>
+        <div className='Header'></div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
