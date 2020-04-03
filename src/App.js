7641b5ee55014d4cbea7b9c6604e7d293c6d6187
Cameron Reed <cameronreedadjusting@gmail.com>
2020-04-03 09:34:28 -0700

    sketching out components and adding to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Dashboard from './Components/Dashboard';
+import Form from './Components/Form';
+import Header from './Components/Header';
+import Product from './Components/Product';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Dashboard'></div>
+      <div className='Form'></div>
+      <div className='Header'></div>
+      <div className='Product'></div>
     </div>
   );
 }
